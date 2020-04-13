63e17841821086e3a1ec2896f465713aa4eca2b0
brody-rebne <brody-rebne@BRODYDESK>
2020-04-13 16:38:58 -0700

    added structure for activity object

--- BoPeepMVC/BoPeepMVC/Models/Activity.cs
+++ BoPeepMVC/BoPeepMVC/Models/Activity.cs
@@ -8,7 +8,31 @@ namespace BoPeepMVC.Models
 {
     public class Activity
     {
-        public string Keyword { get; set; }
-        public string ApiResponse { get; set; }
+        //legacy code for testing routes
+        //public string Keyword { get; set; }
+        //public string ApiResponse { get; set; }
+
+        //db id of activity
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        //user's existing rating
+        //will only be necessary once users are implemented in some way
+        public int Rate { get; set; }
+
+        //aggregate rating
+        public double Rating { get; set; }
+
+        //whether inside or outside
+        public string Location { get; set; }
+
+        //link to external article/source/purchase page
+        public string ExternalLink { get; set; }
+
+        //url for header/icon image
+        public string ImageURL { get; set; }
     }
 }
